ea52dbf35e874186868dfe9675097798456995f3
crigaud <c.rigaud@gmx.com>
2014-10-16 13:33:00 +0200

    + OPU talk 2014 init

--- 2014/JOURNAL/code/evaluator.py
+++ 2014/JOURNAL/code/evaluator.py
@@ -654,4 +654,17 @@ def is_empty(r):
 #
 def rect2geom((left, top), (right, bottom)):
     return (left, top), (right-left, bottom-top)
+    
+alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYabcdefghijklmnopqrstuvwxyz'
+
+def edits1(word):
+   splits     = [(word[:i], word[i:]) for i in range(len(word) + 1)]
+   deletes    = [a + b[1:] for a, b in splits if b]
+   transposes = [a + b[1] + b[0] + b[2:] for a, b in splits if len(b)>1]
+   replaces   = [a + c + b[1:] for a, b in splits for c in alphabet if b]
+   inserts    = [a + c + b     for a, b in splits for c in alphabet]
+   return set(deletes + transposes + replaces + inserts)
+
+def edits2(word):
+    return set(e2 for e1 in edits1(word) for e2 in edits1(e1))
 
---
+++ 2014/TALK/intelligent_media_processing_group/README.md
@@ -0,0 +1,6 @@
+Detection of comic characters in comic books and manga
+======================
+October 17, 2014   
+Osaka Prefecture University - Intelligent Media Processing Group
+
+Pending image copyrights...
